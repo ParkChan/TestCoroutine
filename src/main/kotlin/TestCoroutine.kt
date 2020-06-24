f0684256d2cfd758973a7ff0e0177e33dd1ea5c8
ParkChan <hydrarkk@naver.com>
2020-06-25 00:55:10 +0900

    테스트

---
+++ src/main/kotlin/TestCoroutine.kt
@@ -0,0 +1,24 @@
+import kotlinx.coroutines.*
+import java.lang.Exception
+
+fun main(args: Array<String>) = runBlocking<Unit> {
+
+    launch {
+        try {
+            coroutineScope {
+                launch {
+                    delay(Long.MAX_VALUE)
+                }
+            }
+        } catch (ex: CancellationException) {
+            println(ex)
+        } catch (e: Exception) {
+            println(e)
+        } finally {
+
+        }
+    }
+    println("취소")
+    delay(1000L)
+    coroutineContext[Job]?.cancelChildren()
+}
